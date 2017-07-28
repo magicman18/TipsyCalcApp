2318048afcf613876a59141899a10338b06dc6b4
Larsen Eisenberg <larseneisenberg@larsens-imac.local>
2017-07-28 15:29:12 -0400

    Initial Commit

---
+++ TipsyCalcApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipsyCalcApp
+//
+//  Created by Larsen Eisenberg on 7/28/17.
+//  Copyright © 2017 Magic By Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
