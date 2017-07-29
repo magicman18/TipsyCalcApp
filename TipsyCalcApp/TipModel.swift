f80b89c21ee8bcd58dc7378683628338cb44b3bc
Larsen Eisenberg <larseneisenberg@larsens-imac.local>
2017-07-29 10:16:02 -0400

    Created Project

---
+++ TipsyCalcApp/TipModel.swift
@@ -0,0 +1,50 @@
+//
+//  TipModel.swift
+//  TipsyCalcApp
+//
+//  Created by Larsen Eisenberg on 7/29/17.
+//  Copyright © 2017 Magic By Larsen. All rights reserved.
+//
+
+import Foundation
+
+class TipModel {
+    private var _billAmount: Double = 0
+    private var _tipPercent: Double = 0
+    private var _tipAmount: Double = 0
+    private var _totalAmount: Double = 0
+    
+    var billAmount: Double {
+        get {
+            return _billAmount
+        } set {
+            _billAmount = newValue
+        }
+    }
+    
+    var tipPercent: Double {
+        get {
+            return _tipPercent
+        } set {
+            _tipPercent = newValue
+        }
+    }
+    
+    var tipAmount: Double {
+        return _tipAmount
+    }
+    
+    var totalAmount: Double {
+        return _totalAmount
+    }
+    
+    init(billAmount: Double, tipPercent: Double) {
+        self._billAmount = billAmount
+        self._tipPercent = tipPercent
+    }
+    
+    func calculateTip() {
+        _tipAmount = _billAmount * _tipPercent
+        _totalAmount = billAmount + tipAmount
+    }
+}
--- TipsyCalcApp/ViewController.swift
+++ TipsyCalcApp/ViewController.swift
@@ -10,14 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    @IBOutlet weak var tipPercentageLbl: UILabel!
+    @IBOutlet weak var tipLbl: UILabel!
+    @IBOutlet weak var totalLbl: UILabel!
+    
+    var tip = TipModel(billAmount: 0.0, tipPercent: 0.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setTipCalculationValues()
+        updateUI()
+        
+    }
+    
+    func setTipCalculationValues() {
+        tip.tipPercent = Double(tipPercentageSlider.value)
+        tip.billAmount = ((textField.text)! as NSString).doubleValue
+        tip.calculateTip()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateUI() {
+        tipLbl.text = String(format: "$%0.2f", tip.tipAmount)
+        totalLbl.text = String(format: "$%0.2f", tip.totalAmount)
+        tipPercentageLbl.text = "Tip: \(Int(tipPercentageSlider.value * 100))%"
+    }
+    
+    @IBAction func billAmountWasChanged(_ sender: Any) {
+        setTipCalculationValues()
+        updateUI()
+        print("My value changed!")
+    }
+    
+    @IBAction func tipPercentageDidChange(_ sender: UISlider) {
+        let steps: Float = 100
+        let roundedValue = round(sender.value * steps) / steps
+        sender.value = roundedValue
+        
+        setTipCalculationValues()
+        updateUI()
+        print(tipPercentageSlider.value)
     }
 
 
